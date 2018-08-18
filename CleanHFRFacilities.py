fb82b5802467a882f0b616cdbce4dfa779ed1462
cainam <walterplatnumz@gmail.com>
2018-08-18 14:25:58 +0300

    Some modification added to the project

--- CleanHFRFacilities.py
+++ CleanHFRFacilities.py
@@ -1,80 +1,93 @@
 import csv
 import os.path
 import itertools
+import json
 from os import path
 
+
 class CleanHFRFacilities:
-	
-	mheaders = []
-	filetoread = ""
-	filetowrite = "output.csv"
-	recordrows = 0
-	
-	headerCOnfigurations  = { "name": "jumbo" }
-	
-	def __init__(self, filename):
-		self.filetoread = filename
-				
-	def getHeaders(self):
-		localheaders = []
-		with open('./' + self.filetoread, 'r') as fileheadertoread:
-			headers = csv.DictReader(fileheadertoread)
-			headersJSONData = next(headers)
-			
-			for key in headersJSONData.keys():
-				localheaders.append(key)
-		return localheaders
-		
-	def setHeaders(self, localheaders):
-		for i in len(localheaders):
-			self.mheaders[i] = localheaders[i]
-		return true
-	
-	def getAllRecordsAllRowsDataOnce(self):
-		with open('./' + self.filetoread, 'r') as fileAllrecordstoread:
-			records = csv.DictReader(fileAllrecordstoread)
-			
-			for rowData in records:
-				self.cleanHeaderNames(rowData)
-				
-	def writeIntoNewFile(self, recordDatas):
-		with open('./' + self.filetowrite, 'w') as fileAllrecordstowrite:			
-			writerOBJ = csv.DictWriter(fileAllrecordstowrite, fieldnames=self.mheaders)
-			writerOBJ = csv.writer(fileAllrecordstowrite)
-			writerOBJ.writerow(self.mheaders)
-			keyvalueArray = []
-			for rowData in recordDatas:
-				for i in range(len(self.mheaders)):
-					#keyvalueArray.append(rowData[i])
-					print(i)
-					
-				writerOBJ.writerow(keyvalueArray)	
-	
-	def replaceHeaderNames(self, oldValue, newValue, recordData):
-		self.writeIntoNewFile(recordData)
-		
-		
-	def cleanHeaderNames(self, headerData):
-		for oldkey, oldvalue in headerData.iteritems():
-			for newkey, newvalue in self.headerCOnfigurations.iteritems():
-				if oldkey == newkey:
-					self.replaceHeaderNames(oldvalue, newvalue, headerData)
-	
-					
-	
-		
-	
+    mHeaders = []
+    mNewHeaders = []
+    fileToRead = ""
+    fileToWrite = "output.csv"
+    totalrec = 0;
+    tmpArrayForData = []
+
+    headerConfigurations = {"Ownership": "jumbo", "name": "Malaya", "Usenge": "lat"}
+
+    def __init__(self, filename_path):
+        self.fileToRead = filename_path
+        self.totalrec = self.getTotalRecords()
+        self.mHeaders = self.getHeaderFromFile()
+        self.mNewHeaders = self.cleaningHeaderName()
+        # self.writeIntoNewFile()
+        self.tmpArrayForData = self.getAllRecords()
+
+    def getHeaderFromFile(self):
+        localheaders = []
+        if os.path.exists(self.fileToRead):
+            with open('./' + self.fileToRead, 'r') as fileheaderstoread:
+                headers = csv.DictReader(fileheaderstoread)
+                headerJSON = headers.fieldnames
+                localheaders = headerJSON
+                return localheaders
+        else:
+            print "File doesn't exist, please check the file again"
+
+    def cleaningHeaderName(self):
+        localnewheaders = []
+        for key, value in self.headerConfigurations.iteritems():
+            for i in range(len(self.mHeaders)):
+                if(key == self.mHeaders[i]):
+                    localnewheaders.append(value)
+                else:
+                    localnewheaders.append(self.mHeaders[i])
+
+        return localnewheaders
+
+    def writeIntoNewFile(self):
+        with open('./' + self.fileToWrite, 'w') as filetowrite:
+            writerOBJ = csv.DictWriter(filetowrite, fieldnames=self.mNewHeaders)
+            writerOBJ = csv.writer(filetowrite)
+            writerOBJ.writerow(self.mNewHeaders)
+            # writerOBJ.writerow(newrow)
+
+    def printJSON(self, listofitems):
+        return json.dumps(listofitems, indent=4, sort_keys=True)
 
 
-			
-HFRInstance = CleanHFRFacilities('cainam.csv')
-HFRInstance.mheaders = HFRInstance.getHeaders()
-HFRInstance.getAllRecordsAllRowsDataOnce()
+    def getAllRecords(self):
+        with open('./' + self.fileToRead, 'r') as filedatatoread:
+            records = csv.DictReader(filedatatoread)
 
-#print(HFRInstance.filetoread)
-#print(HFRInstance.getRecordsSingleRowData())
-#print(HFRInstance.getRecordsSingleRowData())
-#print(HFRInstance.filetoread)
-#print("key: {} | value: {}".format(key, value))
+            with open('./' + self.fileToWrite, 'w') as filetowrite:
+                writerOBJ = csv.DictWriter(filetowrite, fieldnames=self.mNewHeaders)
+                writerOBJ = csv.writer(filetowrite)
+                writerOBJ.writerow(self.mNewHeaders)
 
+            for rowData in records:
+                localrecord = []
+                for i in range(len(self.mHeaders)):
+                    # print self.printJSON(rowData[self.mHeaders[i]])
+                    # self.tmpArrayForData.append(rowData[self.mHeaders[i]])
+                    localrecord.append(rowData[self.mHeaders[i]])
+
+                # print localrecord
+
+                writerOBJ.writerow(localrecord)
+                # self.writeIntoNewFile(localrecord)
+
+    def getTotalRecords(self):
+        with open('./' + self.fileToRead, 'r') as filedatatoread:
+            records = csv.DictReader(filedatatoread)
+            counter = 1
+            for rowData in records:
+                counter = counter + 1
+            return counter
+
+
+
+
+objInstance = CleanHFRFacilities('cainam.csv')
+print objInstance.tmpArrayForData
 
---
+++ CleanHFRFacilities2.py
@@ -0,0 +1,82 @@
+# import csv
+# import os.path
+# import itertools
+# import json
+# from os import path
+#
+#
+# class CleanHFRFacilities:
+#     mheaders = []
+#     mNewHeaders = []
+#     filetoread = ""
+#     filetowrite = "output.csv"
+#     recordrows = 0
+#
+#     headerCOnfigurations = {"name": "jumbo"}
+#
+#     def __init__(self, filename):
+#         self.filetoread = filename
+#
+#     def getHeaders(self):
+#         localheaders = []
+#         with open('./' + self.filetoread, 'r') as fileheadertoread:
+#             headers = csv.DictReader(fileheadertoread)
+#             headersJSONData = next(headers)
+#
+#             for key in headersJSONData.keys():
+#                 localheaders.append(key)
+#         return localheaders
+#
+#     def getJSONData(self, listofitems):
+#         return json.dumps(listofitems, indent=4, sort_keys=True)
+#
+#     def getLengthOfItems(self, listofitems):
+#         return len(listofitems)
+#
+#     def setHeaders(self, localheaders):
+#         for i in range(len(localheaders)):
+#             self.mheaders[i] = localheaders[i]
+#         return true
+#
+#     def getAllRecordsAllRowsDataOnce(self):
+#         with open('./' + self.filetoread, 'r') as fileAllrecordstoread:
+#             records = csv.DictReader(fileAllrecordstoread)
+#
+#             for rowData in records:
+#                 print (rowData)
+#                 self.cleanHeaderNames(rowData)
+#
+#     def writeIntoNewFile(self, recordDatas):
+#         with open('./' + self.filetowrite, 'w') as fileAllrecordstowrite:
+#             writerOBJ = csv.DictWriter(fileAllrecordstowrite, fieldnames=self.mheaders)
+#             writerOBJ = csv.writer(fileAllrecordstowrite)
+#             writerOBJ.writerow(self.mheaders)
+#             keyvalueArray = []
+#             for rowData in recordDatas:
+#                 pass
+#             # for i in range(len(self.mheaders)):
+#             # keyvalueArray.append(rowData[i])
+#             # writerOBJ.writerow(keyvalueArray)
+#             # print(rowData['name'])
+#
+#     def replaceHeaderNames(self, oldValue, newValue, recordData):
+#
+#         self.writeIntoNewFile(recordData)
+#
+#     def cleanHeaderNames(self, headerData):
+#         for oldkey, oldvalue in headerData.iteritems():
+#             for newkey, newvalue in self.headerCOnfigurations.iteritems():
+#                 print(oldkey, oldvalue)
+#                 if oldkey == newkey:
+#                     self.replaceHeaderNames(oldvalue, newvalue, headerData)
+#
+#
+# HFRInstance = CleanHFRFacilities('cainam.csv')
+# HFRInstance.mheaders = HFRInstance.getHeaders()
+# HFRInstance.getAllRecordsAllRowsDataOnce()
+#
+# # print(HFRInstance.filetoread)
+# # print(HFRInstance.getRecordsSingleRowData())
+# # print(HFRInstance.getRecordsSingleRowData())
+# # print(HFRInstance.filetoread)
+# # print("key: {} | value: {}".format(key, value))
